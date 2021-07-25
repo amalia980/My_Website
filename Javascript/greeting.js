c33c9c29db6fe2b7ab6099c6c49465a37fbd85e5
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-07-25 17:39:02 +0200

    added language selection

---
+++ Javascript/greeting.js
@@ -0,0 +1,11 @@
+const text = document.getElementById("greeting");
+
+const myArray = ["Hello", "Bonjour", "Hola"];
+
+function greeting() {
+    for (i = 0; i < myArray.length; i++) {
+        //console.log(myArray[i]);
+        //text.innerText = myArray[i];
+    }
+}
+greeting();
